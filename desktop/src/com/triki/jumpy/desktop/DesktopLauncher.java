3912b30bc96b1cad170ad943d60f47cd4d3d5993
Jonas Triki <kosi_722@hotmail.com>
2016-10-02 21:40:11 +0200

    Added LibGdx project

---
+++ desktop/src/com/triki/jumpy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.triki.jumpy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.triki.jumpy.Jumpy;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Jumpy(), config);
+	}
+}
